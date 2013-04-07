4111a392113481b8b6f70722870a6f707f930d48
Sergey Zabolotnov <sergey.zabolotnov@gmail.com>
2013-04-08 01:25:33 +0400

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleTaskManager::Application.config.secret_token = '165646db2cebae7bcc789d198f680c68891ab7dafd602bff25642ef90de7733085e9be0133e43c2fbc35c9115e2807a23897170d410357bf0229916dbcbe5dde'

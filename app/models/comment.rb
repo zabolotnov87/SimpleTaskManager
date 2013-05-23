7b0a353ce15dd4182c899fc558b1815e024bf8c0
Sergey Zabolotnov <sergey.zabolotnov@gmail.com>
2013-05-23 19:32:32 +0400

    Refactor: validates.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :task
 
-  validates :user_id, :task_id, :body, presence: true
+  validates :user, :task, :body, presence: true
 end

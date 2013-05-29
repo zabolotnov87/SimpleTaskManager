1ee7f73d4ce7a2bc1f9457b16176f9dac977b87d
Sergey Zabolotnov <sergey.zabolotnov@gmail.com>
2013-05-29 17:45:28 +0400

    Refactor: move date formatting into locales

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,10 @@
 module ApplicationHelper
-  def format_date(date)
-    date.strftime("%d %b %H:%M")
-  end
 
   def owner_name(task)
     task.has_owner? ? task.user.email : '<none>'
   end
 
   def state(task)
-    task.accepted? ? "accepted at #{format_date(task.accepted_at)}" : task.state
+    task.accepted? ? "accepted at #{l task.accepted_at}" : task.state
   end
 end

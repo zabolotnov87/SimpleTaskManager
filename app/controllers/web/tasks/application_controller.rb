8b2d6e69493defd4f6311ff0e20b7a0ad79066f3
Sergey Zabolotnov <sergey.zabolotnov@gmail.com>
2013-05-29 17:14:35 +0400

    Refactor: a little improvement comment_controller

--- app/controllers/web/tasks/application_controller.rb
+++ app/controllers/web/tasks/application_controller.rb
@@ -1,8 +1,6 @@
 class Web::Tasks::ApplicationController < Web::ApplicationController
 
-  before_filter :get_task
-
-  def get_task
-    @task = Task.find params[:task_id]
+  def current_task
+    Task.find params[:task_id]
   end
 end
--- app/controllers/web/tasks/comments_controller.rb
+++ app/controllers/web/tasks/comments_controller.rb
@@ -1,11 +1,11 @@
 class Web::Tasks::CommentsController < Web::Tasks::ApplicationController
-  
+
   def create
-    @comment = @task.comments.new(params[:comment])
-    @comment.user = current_user
-    if @comment.save
+    comment = current_task.comments.new(params[:comment])
+    comment.user = current_user
+    if comment.save
       flash_success
-      redirect_to :back 
+      redirect_to :back
     else
       flash_error
       redirect_to :back
